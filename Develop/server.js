8a3737f4bd8fd5efc0b95d13deab87affab0f23b
Connor Savoy <connorsavoy812@gmail.com>
2023-02-16 21:38:58 -0500

    removed exccess code from server.js

--- Develop/server.js
+++ Develop/server.js
@@ -22,30 +22,5 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-
-// Get Route for individual note
-app.get('/api/note/:note_id', (req, res) => {
-  if(req.params.note_id){
-    // console.info(`${req.method} request received to get a single a note`);
-    const noteId = req.params.note_id
-    readFromFile('./db/db.json')
-      .then((data) => {
-        const parsedData = JSON.parse(data)
-        for(let i=0; i < parsedData.length; i++){
-          const currentNote = parsedData[i]
-          // console.log(currentNote.note_id + noteId);
-          if(currentNote.note_id === noteId){
-            res.status(200).json(currentNote);
-            // console.log(currentNote);
-            return;
-          }
-        }
-        res.status(404).send('Note ID not found');
-      })
-  } else {
-    res.status(400).send('Note ID not provided');
-  }
-})
-
 app.listen(port, () => 
 console.log(`App listening at http://localhost:${port}`));
